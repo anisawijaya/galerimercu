c45cd1feb0ee4f227e9d0b7a33d47db5530fec09
anisawijaya <Anisawijaya530@gmail.com>
2025-01-19 23:19:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <div class="slide" id="slide1">
             <h2>Welcome to Galerimercu</h2>
             <p>We don't remember days, we remember moments</p>
-            <img src="images/logo.png" width="350"alt="Design 2">
+            <img src="logo.png" width="350"alt="Design 2">
         </div>
 
         <!-- Slide 2 -->
@@ -24,7 +24,7 @@ Dikemas dengan desain yang modern dan navigasi yang mudah, Galerimercu bukan sek
 
 <p style="text-align:center">Temukan kenangan berharga Anda di Galerimercu, dan rasakan kembali indahnya masa-masa di kampus!</p>
 <video controls width="400" height="250">
-  <source src="images/animasi.mp4" type="video/mp4">
+  <source src="animasi.mp4" type="video/mp4">
   Your browser does not support the video tag.
 </video>
 
@@ -34,11 +34,11 @@ Dikemas dengan desain yang modern dan navigasi yang mudah, Galerimercu bukan sek
         <div class="slide" id="slide3">
             <h2>Design</h2>
             <p>berikut ini adalah tampilan dari proyek dalam website ini.</p>
-            <img src="images/homescreen.png" width="200"alt="Design 1">
-            <img src="images/homescreen2.png" width="200"alt="Design 2">
-            <img src="images/wisuda.png" width="200"alt="Design 3">
-            <img src="images/wisuda2.png" width="200"alt="Design 4">
-            <img src="images/wisuda3.png" width="200"alt="Design 5">
+            <img src="homescreen.png" width="200"alt="Design 1">
+            <img src="homescreen2.png" width="200"alt="Design 2">
+            <img src="wisuda.png" width="200"alt="Design 3">
+            <img src="wisuda2.png" width="200"alt="Design 4">
+            <img src="wisuda3.png" width="200"alt="Design 5">
 
         </div>
 
@@ -46,7 +46,7 @@ Dikemas dengan desain yang modern dan navigasi yang mudah, Galerimercu bukan sek
         <div class="slide" id="slide4">
             <h2>Pembuat Program</h2>
             <p>Program ini dibuat oleh Arya Puji Asmoro, Mahasiswa di STMIK Mercusuar.</p>
-            <img src="images/gweh.jpg" width="300"alt="Design 7">
+            <img src="gweh.jpg" width="300"alt="Design 7">
         </div>
 
         <!-- Navigation Buttons -->
